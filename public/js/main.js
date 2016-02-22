dbc200d5b62fddee25cbc3045bc904c8ed467374
Alexei Volkov <lifecoder.ua@gmail.com>
2016-02-22 10:57:51 +0200

    gulp watch

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ require('angular')
 var app = angular.module('app', [])
 
 app.controller('MainController', function($scope) {
-    $scope.message = 'Angular Works!'
+    $scope.message = 'Angular 1 Works!'
 })
 },{"angular":3}],2:[function(require,module,exports){
 /**
