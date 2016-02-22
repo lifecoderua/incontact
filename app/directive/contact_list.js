6ea8ad82d822c51c34f3b62b3fa898b248c58817
Alexei Volkov <lifecoder.ua@gmail.com>
2016-02-22 17:43:55 +0200

    view mode logic

--- app/directive/contact_list.js
+++ app/directive/contact_list.js
@@ -8,7 +8,7 @@ function contactList() {
   var directive = {
     templateUrl: '/templates/contact_list.html',
     controller: ContactListController,
-    controllerAs: 'contact_list',
+    controllerAs: 'contactList',
     bindToController: true
   };
 
@@ -19,11 +19,12 @@ function ContactListController() {
   var vm = this;
 
   // vm.view_modes = ['list', 'grid'];
-  vm.view_mode = 'list';
-
+  vm.viewMode = 'list';
   vm.contacts = [
     {id: 5, first_name: 'Test', last_name: 'Me'},
     {id: 2, first_name: 'Test2', last_name: 'Me'},
     {id: 3, first_name: 'Test', last_name: 'Me3'}
   ];
+  vm.setViewMode = function(mode) { vm.viewMode = mode };
+
 }
