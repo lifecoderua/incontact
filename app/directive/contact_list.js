43a9ce4ee70dc75ce9e0741e07162f5d6c0c1370
Alexei Volkov <lifecoder.ua@gmail.com>
2016-02-23 13:18:27 +0200

    real localStorage listing

--- app/directive/contact_list.js
+++ app/directive/contact_list.js
@@ -20,14 +20,12 @@ function ContactListController(contactService) {
 
   // vm.view_modes = ['list', 'grid'];
   vm.viewMode = 'list';
-  vm.contacts = [
-    {id: 5, first_name: 'Test', last_name: 'Me'},
-    {id: 2, first_name: 'Test2', last_name: 'Me'},
-    {id: 3, first_name: 'Test', last_name: 'Me3'},
-    {id: 9, first_name: 'Another', last_name: 'Me3'},
-    {id: 4, first_name: 'Test', last_name: 'One'},
-    contactService.find()
-  ];
   vm.setViewMode = function(mode) { vm.viewMode = mode };
+  vm.contacts = [];
 
+  activate();
+
+  function activate() {
+    vm.contacts = contactService.all();
+  }
 }
--- app/service/contact.js
+++ app/service/contact.js
@@ -8,23 +8,30 @@ contactService.$inject = ['localStorageService'];
 function contactService(localStorageService) {
   var service = {
     find: find,
-    save: save
+    save: save,
+    destroy: destroy,
+    all: all
   };
 
   return service;
 
-
   function find(id) {
-    return localStorageService.get(id);
-    //{
-    //  id: id,
-    //  first_name: 'Another',
-    //  last_name: 'Contact'
-    //}
+    return all()[id];
   }
 
   function save(contact) {
-    localStorageService.set(contact.id, contact);
-    console.log('saved')
+    var contacts = all();
+    contacts[contact.id] = contact;
+    localStorageService.set('contacts', contacts);
+  }
+
+  function all() {
+    return localStorageService.get('contacts') || {};
+  }
+
+  function destroy(id) {
+    var contacts = all();
+    contacts[contact.id] = null;
+    localStorageService.set('contacts', contacts);
   }
 }
